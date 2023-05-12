4b9eeb0ffbc4c0c96b6eef62e4d283b2d62451d2
Rizco Renova <rizcorenova31@gmail.com>
2023-05-12 10:59:33 +0700

    add dynamic size for font

--- lib/common/style/font_style.dart
+++ lib/common/style/font_style.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppFont {
   static final largeText = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500);
+      color: Colors.white, fontSize: 16.sp, fontWeight: FontWeight.w500);
   static final smallText = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 12, fontWeight: FontWeight.w500);
+      color: Colors.white, fontSize: 12.sp, fontWeight: FontWeight.w500);
   static final mediumText = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500);
+      color: Colors.white, fontSize: 14.sp, fontWeight: FontWeight.w500);
   static final subtitle = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold);
+      color: Colors.white, fontSize: 16.sp, fontWeight: FontWeight.bold);
   static final boldMediumText = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold);
+      color: Colors.white, fontSize: 14.sp, fontWeight: FontWeight.bold);
   static final boldSmallText = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold);
+      color: Colors.white, fontSize: 12.sp, fontWeight: FontWeight.bold);
   static final headline6 = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold);
+      color: Colors.white, fontSize: 20.sp, fontWeight: FontWeight.bold);
   static final headline4 = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold);
+      color: Colors.white, fontSize: 32.sp, fontWeight: FontWeight.bold);
   static final headline5 = GoogleFonts.poppins(
-      color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold);
+      color: Colors.white, fontSize: 24.sp, fontWeight: FontWeight.bold);
 }
